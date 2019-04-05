de8e9fbb421c542d36afbb0c7ad67f5c7527c45e
zaslab <itskov@gmail.com>
2019-04-05 03:52:50 +0300

    Dude3

--- aeConnectivity.py
+++ aeConnectivity.py
@@ -5,30 +5,30 @@ import os
 
 def createNetwork(activationInput, y, N, T):
     conv1 = tf.layers.conv2d(inputs = activationInput,
-                             filters = 16,
-                             kernel_size = [5,1],
+                             filters = 32,
+                             kernel_size = [2,1],
                              padding = "same",
                              activation=tf.nn.relu)
 
     conv2 = tf.layers.conv2d(inputs = conv1,
                              filters = 32,
+                             kernel_size = [3,1],
+                             padding = "same",
+                             activation=tf.nn.relu)
+
+    #conv3 = tf.layers.conv2d(inputs = conv2,
+    #                         filters = 64,
+    #                         kernel_size = [5,1],
+    #                         padding = "same",
+    #                         activation=tf.nn.relu)
+
+    conv4 = tf.layers.conv2d(inputs = conv2,
+                             filters = 5,
                              kernel_size = [5,1],
                              padding = "same",
                              activation=tf.nn.relu)
 
-    conv3 = tf.layers.conv2d(inputs = conv2,
-                             filters = 64,
-                             kernel_size = [5,1],
-                             padding = "same",
-                             activation=tf.nn.relu)
-
-    conv4 = tf.layers.conv2d(inputs = conv3,
-                             filters = 3,
-                             kernel_size = [5,1],
-                             padding = "same",
-                             activation=tf.nn.relu)
-
-    conv4_flat = tf.reshape(conv4, (-1, 3 * (N * T)))
+    conv4_flat = tf.reshape(conv4, (-1, 5 * (N * T)))
     dense1 = tf.layers.dense(inputs=conv4_flat, units= N*N, activation=tf.nn.relu)
     dense2 = tf.layers.dense(inputs=dense1, units=N * N)
 
@@ -36,12 +36,20 @@ def createNetwork(activationInput, y, N, T):
 
     loss = tf.reduce_mean(tf.norm(output - y, axis=(1,2)))
 
-    return (conv1, conv2, conv3, conv4, dense1, dense2, output, loss, y)
+    return (conv1,
+            conv2,
+            #conv3,
+            conv4,
+            dense1,
+            dense2,
+            output,
+            loss,
+            y)
 
 
 
 def getTrainBatch(activationMatrices, connectivityMatrices):
-    batchSize = 5
+    batchSize = 20
 
     trainSize = activationMatrices.shape[0]
 
@@ -70,7 +78,7 @@ def main():
         currentActivation_ph = tf.placeholder(tf.float64, (None, T, N, 1), name="xPh")
         currentConnectivity_ph = tf.placeholder(tf.float64, (None, N, N, 1), name="yPh")
 
-        (conv1, conv2, conv3, conv4, dense1, dense2, output, loss, y) = \
+        (conv1, conv2, conv4, dense1, dense2, output, loss, y) = \
             createNetwork(currentActivation_ph, currentConnectivity_ph, N,T)
 
         optimizer = tf.train.AdamOptimizer().minimize(loss)
